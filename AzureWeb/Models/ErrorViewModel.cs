8636a0719bf662f2f0707d2b27d63f6def106f6c
arthu <arthu@DESKTOP-871PQNN>
2023-02-28 20:32:07 -0300

    Adicionar arquivos de projeto.

---
+++ AzureWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
